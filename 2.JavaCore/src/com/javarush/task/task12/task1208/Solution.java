9658a1d3336a43227268ee63c5a4219aea8664c6
tolymhlv <tolymhlv@gmail.com>
2019-02-22 17:45:29 +0300

    another lessons commit

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(int i) {
+
+    }
+
+    public static void print(Integer i) {
+
+    }
+
+    public static void print(String s) {
+
+    }
+
+    public static void print(int x, int y) {
+
+    }
+
+    public static void print(Boolean b) {
+
+    }
+    //Напишите тут ваши методы
+}
---
+++ 2.JavaCore/src/com/javarush/task/task12/task1210/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task12.task1210;
+
+/* 
+Три метода и максимум
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    public static int max(int x, int y) {
+        return x < y ? y : x;
+    }
+
+    public static long max(long x, long y) {
+        return x < y ? y : x;
+    }
+
+    public static double max(double x, double y) {
+        return x < y ? y : x;
+    }
+    //Напишите тут ваши методы
+}
