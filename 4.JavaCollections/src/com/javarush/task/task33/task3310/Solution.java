09b23be438ac6df15940fc5a8e55a30740fbff3b
uncletolya <stolikzakazan@gmail.com>
2019-03-11 23:54:17 +0300

    hard task about hashmap

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
@@ -0,0 +1,63 @@
+package com.javarush.task.task33.task3310;
+
+import com.javarush.task.task33.task3310.strategy.HashMapStorageStrategy;
+import com.javarush.task.task33.task3310.strategy.OurHashMapStorageStrategy;
+import com.javarush.task.task33.task3310.strategy.StorageStrategy;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+
+
+    public static void main(String[] args) {
+        testStrategy(new HashMapStorageStrategy(), 10000);
+        testStrategy(new OurHashMapStorageStrategy(), 10000);
+    }
+
+    public static Set<Long> getIds(Shortener shortener, Set<String> strings) {
+        Set<Long> ids = new HashSet<>();
+        for (String string : strings) {
+            ids.add(shortener.getId(string));
+        }
+        return ids;
+    }
+
+    public static Set<String> getStrings(Shortener shortener, Set<Long> keys) {
+        Set<String> strings = new HashSet<>();
+        for (Long key : keys) {
+            strings.add(shortener.getString(key));
+        }
+        return strings;
+    }
+
+    public static void testStrategy(StorageStrategy strategy, long elementsNumber) {
+        Helper.printMessage(strategy.getClass().getSimpleName());
+
+        Set<String> randomStrings = new HashSet<>();
+        for (int i = 0; i < elementsNumber; i++)
+            randomStrings.add(Helper.generateRandomString());
+
+        Shortener shortener = new Shortener(strategy);
+
+        Date date1 = new Date();
+        Set<Long> ids = getIds(shortener, randomStrings);
+        Date date2 = new Date();
+        long timeForIds = date2.getTime() - date1.getTime();
+        System.out.println(timeForIds);
+
+        Date date3 = new Date();
+        Set<String> strings = getStrings(shortener, ids);
+        Date date4 = new Date();
+        long timeForStrings = date4.getTime() - date3.getTime();
+        System.out.println(timeForStrings);
+
+        if (ids.size() == strings.size())
+            System.out.println("Тест пройден.");
+        else
+            System.out.println("Тест не пройден.");
+
+
+    }
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/strategy/OurHashMapStorageStrategy.java
@@ -0,0 +1,139 @@
+package com.javarush.task.task33.task3310.strategy;
+
+public class OurHashMapStorageStrategy implements StorageStrategy {
+    static final int DEFAULT_INITIAL_CAPACITY = 16;
+    static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    Entry[] table = new Entry[DEFAULT_INITIAL_CAPACITY];
+    int size;
+    int threshold = (int) (DEFAULT_INITIAL_CAPACITY * DEFAULT_LOAD_FACTOR);
+    float loadFactor = DEFAULT_LOAD_FACTOR;
+
+    public int hash(Long k) {
+        k ^= (k >>> 20) ^ (k >>> 12);
+        return (int)(k ^ (k >>> 7) ^ (k >>> 4));
+    }
+    public int indexFor(int hash, int length) {
+        return hash & (length - 1);
+    }
+
+
+    public void resize(int newCapacity) {
+        if (table.length == DEFAULT_INITIAL_CAPACITY)
+        {
+            threshold = Integer.MAX_VALUE;
+            return;
+        }
+
+        Entry[] newTable = new Entry[newCapacity];
+        transfer(newTable);
+        table = newTable;
+        threshold = (int)(newCapacity * loadFactor);
+    }
+    public void transfer(Entry[] newTable) {
+        Entry[] src = table;
+        int newCapacity = newTable.length;
+        for (int j = 0; j < src.length; j++) {
+            Entry e = src[j];
+            if (e != null) {
+                src[j] = null;
+                do {
+                    Entry next = e.next;
+                    int i = indexFor(e.hash, newCapacity);
+                    e.next = newTable[i];
+                    newTable[i] = e;
+                    e = next;
+                } while (e != null);
+            }
+        }
+    }
+
+    public Entry getEntry(Long key) {
+        for (Entry entry : table) {
+            if (entry.key.equals(key)) return entry;
+            Entry next;
+            while ((next = entry.next) != null) {
+                if (next.key.equals(key)) return next;
+            }
+        }
+        return null;
+    }
+
+    public void addEntry(int hash, Long key, String value, int bucketIndex) {
+        Entry e = table[bucketIndex];
+        table[bucketIndex] = new Entry(hash, key, value, e);
+        if (size++ >= threshold)
+            resize(2 * table.length);
+
+    }
+    public void createEntry(int hash, Long key, String value, int bucketIndex) {
+        Entry e = table[bucketIndex];
+        table[bucketIndex] = new Entry(hash, key, value, e);
+        size++;
+    }
+
+    @Override
+    public boolean containsKey(Long key) {
+        int hash = hash((long)key.hashCode());
+        int i = indexFor(hash, table.length);
+        for (Entry e = table[i]; e != null; e = e.next) {
+            Object k;
+            if (e.hash == hash && ((k = e.key) == key || key.equals(k))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(String value) {
+        for (int i = 0; i < table.length ; i++) {
+            for (Entry e = table[i]; e != null; e = e.next) {
+                if (e.value == value || e.value.equals(value)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void put(Long key, String value) {
+//        if (key == null)
+//        return putForNullKey(value);
+        int hash = hash((long)key.hashCode());
+        int i = indexFor(hash, table.length);
+        for (Entry e = table[i]; e != null; e = e.next) {
+            Object k;
+            if (e.hash == hash && ((k = e.key) == key || key.equals(k))) {
+                e.value = value;
+            }
+        }
+        addEntry(hash, key, value, i);
+    }
+
+    @Override
+    public Long getKey(String value) {
+        for (int i = 0; i < table.length ; i++) {
+            for (Entry e = table[i]; e != null; e = e.next) {
+                if (e.value == value || e.value.equals(value)) {
+                    return e.key;
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String getValue(Long key) {
+        int hash = hash((long)key.hashCode());
+        int i = indexFor(hash, table.length);
+        Entry e;
+        for (e = table[i]; e != null; e = e.next) {
+            Object k;
+            if (e.hash == hash && ((k = e.key) == key || key.equals(k))) {
+                return e.value;
+            }
+        }
+        return e.value;
+    }
+}
